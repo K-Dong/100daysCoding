4f372f86faf62af8c33aaa09fa6d5b95c8720211
Kiriku <138890922+K-Dong@users.noreply.github.com>
2024-04-28 18:52:23 +0900

    TicTacToe

---
+++ 100-days-project-Tic_Tac_Toe/scripts/app.js
@@ -0,0 +1,49 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
+let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
+
+const players = [
+  {
+    name: '',
+    symbol: 'X'
+  },
+  {
+    name: '',
+    symbol:'O'
+  },
+];
+
+const playerConfigOverlayElement = document.querySelector('#config-overlay');
+const backdropElement = document.querySelector('#backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.querySelector('#config-errors');
+const gameAreaElement = document.querySelector('#active-game');
+const activePlayerNameElement = document.querySelector('#active-player-name');
+const gameOverElement = document.getElementById('game-over');
+
+const editPlayer1BtnElement = document.querySelector('#edit-player-1-btn');
+const editPlayer2BtnElement = document.querySelector('#edit-player-2-btn');
+const cancelConfigBtnElement = document.querySelector('#cnacel-config-btn');
+const startNewGameBtnElement = document.querySelector('#start-game-btn');
+const gameFieldElements = document.querySelectorAll('#game-board li');
+
+editPlayer1BtnElement.addEventListener('click',openPlayerConfig);
+editPlayer2BtnElement.addEventListener('click',openPlayerConfig);
+
+cancelConfigBtnElement.addEventListener('click',closePlayerConfig);
+backdropElement.addEventListener('click',closePlayerConfig);
+
+formElement.addEventListener('submit',savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click',startNewGame);
+
+for (const gameFieldElement of gameFieldElements) {
+  gameFieldElement.addEventListener('click',selectGameField);
+}
